d37276d7f8740b3cde8efb131703fa452ef1f123
Davi Barbosa <davibes@al.insper.edu.br>
2025-10-23 16:11:51 -0300

    preenche_frota

--- funcoes.py
+++ funcoes.py
@@ -10,3 +10,13 @@ def define_posicoes(i , j , o , t):
         for k in range(t):
             lista.append((i , j + k)) #posições na horizontal
     return lista
+
+def preenche_frota(frota, nv , i, j, o, t):
+    posicoes = define_posicoes(i, j, o, t)
+
+    if nv in frota:
+        frota[nv].append(posicoes)
+    else:
+        frota[nv] = [posicoes]
+
+    return frota
