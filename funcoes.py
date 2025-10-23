23e3cd90369cb476a048a88d8716a78e0d4ab233
Gabriel Menezes <gabrielsm9@al.insper.edu.br>
2025-10-23 20:29:14 -0300

    preenche_frota5

--- funcoes.py
+++ funcoes.py
@@ -1,19 +1,18 @@
 # Exercício 1:
 
-
-def define_posicoes(i , j , o , t):
+def define_posicoes(i, j, o, t):
     lista = []
     if o == "vertical":
         for k in range(t):
-            lista.append((i + k, j)) #posições na vertical
+            lista.append((i + k, j))    # tupla (linha, coluna)
     elif o == "horizontal":
         for k in range(t):
-            lista.append([i , j + k]) #posições na horizontal
+            lista.append((i, j + k))    # tupla (linha, coluna)
     return lista
 
 # Exercício 2:
 
-def preenche_frota(frota, nv , i, j, o, t):
+def preenche_frota(frota, nv, i, j, o, t):
     posicoes = define_posicoes(i, j, o, t)
 
     if nv in frota:
