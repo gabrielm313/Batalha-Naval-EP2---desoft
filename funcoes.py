99e7723f79ec3b40c194554a0feaf0974a18ac9f
Gabriel Menezes <gabrielsm9@al.insper.edu.br>
2025-10-23 19:50:31 -0300

    preenche_frota3

--- funcoes.py
+++ funcoes.py
@@ -11,6 +11,8 @@ def define_posicoes(i , j , o , t):
             lista.append([i , j + k]) #posições na horizontal
     return lista
 
+# Exercício 2:
+
 def preenche_frota(frota, nv , i, j, o, t):
     posicoes = define_posicoes(i, j, o, t) 
 
@@ -20,6 +22,4 @@ def preenche_frota(frota, nv , i, j, o, t):
         if navio == nv:
             dicio[navio] = posicoes
 
-    return frota
-
-print(preenche_frota({'porta-aviões': [[(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)]], 'navio-tanque': [[(2, 2), (3, 2), (4, 2), (5, 2)]], 'contratorpedeiro': [[(9, 0), (9, 1), (9, 2)]], 'submarino': [[(4, 5), (4, 6)]]}, 'submarino', 6, 0, 'horizontal', 2))
+    return dicio
