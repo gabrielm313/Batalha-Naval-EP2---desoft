25ce670938e8d5b01a1a20843c0ef9c5e58e206a
Gabriel Menezes <gabrielsm9@al.insper.edu.br>
2025-10-23 14:59:13 -0300

    Exercício 1 - final

--- funcoes.py
+++ funcoes.py
@@ -5,8 +5,8 @@ def define_posicoes(i , j , o , t):
     lista = []
     if o == "vertical":
         for k in range(t):
-            lista.append((i + k, j)) #posições na vertical
+            lista.append([i + k, j]) #posições na vertical
     elif o == "horizontal":
         for k in range(t):
-            lista.append((i , j + k)) #posições na horizontal
+            lista.append([i , j + k]) #posições na horizontal
     return lista
