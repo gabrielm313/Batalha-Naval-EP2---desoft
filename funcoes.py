789eaee399dbed32373a0922be6c2be4e89dedc1
Gabriel Menezes <gabrielsm9@al.insper.edu.br>
2025-10-23 23:54:23 -0300

    faz_jogada

--- funcoes.py
+++ funcoes.py
@@ -1,26 +1,4 @@
-# Exercício 1:
-
-def define_posicoes(i, j, o, t):
-    lista = []
-    if o == "vertical":
-        for k in range(t):
-            lista.append([i + k, j])    # tupla (linha, coluna)
-    elif o == "horizontal":
-        for k in range(t):
-            lista.append([i, j + k])    # tupla (linha, coluna)
-    return lista
-
-# Exercício 2:
-
-def preenche_frota(frota, nv, i, j, o, t):
-    posicoes = define_posicoes(i, j, o, t)
-
-    if nv in frota:
-        frota[nv].append(posicoes)
-    else:
-        frota[nv] = [posicoes]
-    return frota
-
+#Exercício 3
 def faz_jogada(tab, i, j):
     if tab[i][j] == 1:
         tab[i][j] = 'X'
