188e43092ed2713a92a35b933aacd77f84be8f79
Davi Barbosa <davibes@al.insper.edu.br>
2025-10-24 21:41:45 -0300

    sim

--- programa.py
+++ programa.py
@@ -8,37 +8,37 @@ frota = {
 
 for nome in frota.keys():
     if nome == "porta-aviões":
-        tamanho = 4
-        quantidade = 1
+        t = 4
+        q = 1
     elif nome == "navio-tanque":
-        tamanho = 3
-        quantidade = 2
+        t = 3
+        q = 2
     elif nome == "contratorpedeiro":
-        tamanho = 2
-        quantidade = 3
+        t = 2
+        q = 3
     elif nome == "submarino":
-        tamanho = 1
-        quantidade = 4
+        t = 1
+        q = 4
 
-    for v in range(quantidade):
+    for v in range(q):
         f = False
         while not f:
-            print(f"Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}")
-            linha = int(input("coloque a linha:"))
-            coluna = int(input("coloque a coluna:"))
+            print(f"Insira as informações referentes ao navio {nome} que possui tamanho {t}")
+            l = int(input("coloque a linha:"))
+            c = int(input("coloque a coluna:"))
 
             if nome != "submarino":
-                orientacao = int(input("coloque a orientação:"))
-                if orientacao == 1:
-                    orientacao = 'vertical'
-                elif orientacao == 2:
-                    orientacao = 'horizontal'
+                o = int(input("coloque a orientação:"))
+                if o == 1:
+                    o = 'vertical'
+                elif o == 2:
+                    o = 'horizontal'
 
-            if posicao_valida(frota, linha, coluna, orientacao, tamanho) == False:
+            if posicao_valida(frota, l, c, o, t) == False:
                 print("Esta posição não está válida!")
             else:
-                define_posicoes(linha, coluna, orientacao, tamanho)
-                frota = preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
+                define_posicoes(l, c, o, t)
+                frota = preenche_frota(frota, nome, l, c, o, t)
                 f = True
 
 print(frota)
