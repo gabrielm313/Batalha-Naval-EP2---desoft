018b2559612bb13a60153c0f5254cdaba9207690
Davi Barbosa <davibes@al.insper.edu.br>
2025-10-24 22:02:43 -0300

    ex7

--- programa.py
+++ programa.py
@@ -1,4 +1,5 @@
 from funcoes import *
+
 frota = {
     "porta-aviões": [],
     "navio-tanque": [],
@@ -6,39 +7,49 @@ frota = {
     "submarino": [],
 }
 
+# tamanhos e quantidades esperadas
+tamanho = {
+    "porta-aviões": 4,
+    "navio-tanque": 3,
+    "contratorpedeiro": 2,
+    "submarino": 1,
+}
+
+quantidade = {
+    "porta-aviões": 1,
+    "navio-tanque": 2,
+    "contratorpedeiro": 3,
+    "submarino": 4,
+}
+
 for nome in frota.keys():
-    if nome == "porta-aviões":
-        t = 4
-        q = 1
-    elif nome == "navio-tanque":
-        t = 3
-        q = 2
-    elif nome == "contratorpedeiro":
-        t = 2
-        q = 3
-    elif nome == "submarino":
-        t = 1
-        q = 4
+    t = tamanho[nome]
+    q = quantidade[nome]
 
-    for v in range(q):
-        f = False
-        while not f:
+    for u in range(q):
+        colocado = False
+        while not colocado:
             print(f"Insira as informações referentes ao navio {nome} que possui tamanho {t}")
-            i = int(input("linha:"))
-            j = int(input(" coluna:"))
+            i = int(input("Linha: "))
+            j = int(input("Coluna: "))
 
-            if nome != "submarino":
-                o = int(input(" orientação:"))
-                if o == 1:
-                    o = 'vertical'
-                elif o == 2:
-                    o = 'horizontal'
+            # orientacao: use strings 'vertical'/'horizontal' (como suas funções definem)
+            if nome == "submarino":
+                o = "vertical"   # padrão para tamanho 1 (não afeta o resultado)
+            else:
+                o_num = int(input("[1] Vertical [2] Horizontal >"))
+                if o_num == 1:
+                    o = "vertical"
+                else:
+                    o = "horizontal"
 
-            if posicao_valida(frota, i, j, o, t) == False:
+            # verifica posição (posicao_valida espera (frota, i, j, o, t))
+            if not posicao_valida(frota, i, j, o, t):
+                # mensagem EXATA pedida pelo enunciado
                 print("Esta posição não está válida!")
             else:
-                define_posicoes(i, j, o, t)
+                # preenche_frota tem assinatura que você usou: (frota, nv, i, j, o, t)
                 frota = preenche_frota(frota, nome, i, j, o, t)
-                f = True
+                colocado = True
 
 print(frota)
